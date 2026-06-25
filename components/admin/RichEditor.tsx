2bed5094b487bed717013d5e5f6905c0f36309cf
galavizion <rene.galaviz@gmail.com>
2026-06-25 12:06:43 -0600

    fix: RichEditor setContent signature para TipTap v2

--- components/admin/RichEditor.tsx
+++ components/admin/RichEditor.tsx
@@ -25,7 +25,7 @@ export default function RichEditor({ value, onChange }: Props) {
 
   useEffect(() => {
     if (editor && value !== editor.getHTML()) {
-      editor.commands.setContent(value, false);
+      editor.commands.setContent(value, { emitUpdate: false });
     }
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
